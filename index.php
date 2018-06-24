5e278eeb080976c66652db74675a364ec86c89e2
yanzky <yanzkytootsky@gmail.com>
2018-06-24 19:02:28 +0800

    Sign-in,sign-up,Admin template

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    <link href="css/bootstrap-responsive.min.css" rel="stylesheet" type="text/css"/>
+
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,400,600" rel="stylesheet">
+
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="css/pages/signin.css" rel="stylesheet" type="text/css">
+
+</head>
+
+<body>
+
+
+<div class="account-container">
+
+    <div class="content clearfix">
+
+        <form action="#" method="post">
+
+            <div class="text-center">
+                <h1>Member Login</h1>
+            </div>
+
+            <div class="login-fields">
+
+
+                <div class="field">
+                    <label for="username">Username</label>
+                    <input type="text" id="username" name="username" value="" placeholder="Username"
+                           class="login username-field"/>
+                </div> <!-- /field -->
+
+                <div class="field">
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password" value="" placeholder="Password"
+                           class="login password-field"/>
+                </div> <!-- /password -->
+
+            </div> <!-- /login-fields -->
+
+            <div class="pull-left">
+                <button class="button btn btn-success btn-large">Sign Up</button>
+
+            </div> <!-- .actions -->
+            <div class="pull-right">
+                <button class="button btn btn-success btn-large">Sign In</button>
+
+            </div> <!-- .actions -->
+
+
+        </form>
+
+    </div> <!-- /content -->
+
+</div> <!-- /account-container -->
+
+
+<script src="js/jquery-1.7.2.min.js"></script>
+<script src="js/bootstrap.js"></script>
+
+<script src="js/signin.js"></script>
+
+</body>
+
+</html>
